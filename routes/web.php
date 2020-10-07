1ce2f657126c0639174f1dc1a3364ae86df8ff59
unknown <sharmasofiaa@gmail.com git config --global user.email sharmasofiaa@gmail.com>
2020-10-07 18:45:23 +0545

    registration

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
